5d7acc91549da0d6215eb72285b71b6ca858dce4
nahom4 <nahomamare52@gmail.com>
2023-06-15 11:54:01 +0300

    completed

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/person',require('./routes/person'))
 app.use(cors())
-
 app.use((req, res, next) => {
     res.status(404).json({ error: 'Not Found' });
+    
   });
+  app.use((err, req, res, next) => {
+    res.status(500).json({ error: "server error" });
+  });
+  
 
 const PORT = process.env.port || 3000
 if (require.main === module) {
